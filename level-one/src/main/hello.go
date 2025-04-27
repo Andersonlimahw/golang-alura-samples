bbdb97fdd6b80b0fdefa22d741f00857e4b28b4d
andersonlimadev <andersonlimahw@gmail.com>
2025-04-27 18:33:11 -0300

    🆕 feature: adding array, slices, and for samples

--- level-one/src/main/hello.go
+++ level-one/src/main/hello.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 import "os"
+import "net/http"
 
 const (
 	EXIT             = 0
 	START_MONITORING = 1
 	SHOW_LOGS        = 2
+	SHOW_SAMPLES     = 3
 )
 
 func main() {
-	showIntro()
-	showMenu()
-	var command int = getCommand()
-	if !validateCommand(command) {
-		fmt.Println("Comando não reconhecido")
-		return
+	for { // loop infinito pois no go não temos while
+		showIntro()
+		showMenu()
+		var command int = getCommand()
+		if !validateCommand(command) {
+			fmt.Println("Comando não reconhecido")
+			return
+		}
+		handleCommand(command)
 	}
-	handleCommand(command)
+
 }
 
 func showIntro() {
-	var name string = "Lemon"
+	name, _ := getPerson() // o _ ignora o valor retornadothu
 	version := 1.1
 
 	fmt.Println("Olá, Mr", name)
@@ -33,6 +38,7 @@ func showMenu() {
 	fmt.Println("0- Sair do Programa")
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
+	fmt.Println("3- Executar exemplos")
 }
 
 func getCommand() int {
@@ -43,7 +49,7 @@ func getCommand() int {
 
 // validateCommand if sempre deve retornar true ou false e nao usa parenteses
 func validateCommand(command int) bool {
-	return command >= 0 && command <= 2
+	return command >= 0 && command <= 3
 }
 
 func handleCommand(command int) {
@@ -52,11 +58,71 @@ func handleCommand(command int) {
 		fmt.Println("Saindo do programa...")
 		os.Exit(0) // Finaliza o programa com sucesso, para erro é -1
 	case START_MONITORING:
-		fmt.Println("Iniciando monitoramento...")
+		startMonitoring()
 	case SHOW_LOGS:
 		fmt.Println("Exibindo logs...")
+	case SHOW_SAMPLES:
+		fmt.Println("Exibindo exemplos	...")
+		handleSamples()
 	default:
 		fmt.Println("Comando não reconhecido")
 		os.Exit(-1)
+	}
+}
+
+func startMonitoring() {
+	fmt.Println("Iniciando monitoramento...")
+	site := "https://www.google.com"
+	response, err := http.Get(site)
+	if err != nil {
+		fmt.Println("Erro ao acessar o site", site, "Error -> ", err)
+	}
+	fmt.Println("Monitoramento finalizado..., response", response)
+}
+
+func getPerson() (string, int) {
+	name := "Lemon"
+	age := 22
+
+	return name, age
+}
+func handleSamples() {
+	sliceSample()
+	addElement("Mickey")
+	pokerSamples()
+	forSample()
+}
+
+func sliceSample() {
+	// Usamos Slice pq o tamanho da lista pode mudar e se
+	//usarmos um array o valor limit deve se informado  exemplo varr sites[4]string
+	// tudo é um array no Go
+	fmt.Println("slice sample")
+	slice := []string{"Anderson", "Lemon"}
+	fmt.Println(slice)
+	fmt.Println("Tamanho", len(slice))
+	fmt.Println("Capacidade", cap(slice))
+}
+
+func addElement(name string) {
+	fmt.Println("slice sample : add element")
+	slice := []string{"Anderson", "Lemon", name}
+	fmt.Println(slice)
+	slice = append(slice, name)
+	fmt.Println("Slice atualizado com o novo elemento", name)
+	fmt.Println(slice)
+}
+
+func pokerSamples() {
+	fmt.Println("slice sample : capacity")
+	pontosPlanningPoker := []int{1, 2, 3, 5, 8, 13, 21}
+	pontosPlanningPoker = append(pontosPlanningPoker, 40)
+	fmt.Println(cap(pontosPlanningPoker))
+}
+
+func forSample() {
+	fmt.Println("for sample")
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
 	}
 }
